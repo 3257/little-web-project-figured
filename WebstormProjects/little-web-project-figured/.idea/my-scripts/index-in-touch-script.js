b06a63e05034bead066f3346ed4b9cb16b342634
3257 <deyanaaleksandrov@gmail.com>
2016-09-12 11:53:35 +0300

    fix get in touch background, fix footer

--- WebstormProjects/little-web-project-figured/.idea/my-scripts/index-in-touch-script.js
+++ WebstormProjects/little-web-project-figured/.idea/my-scripts/index-in-touch-script.js
@@ -3,9 +3,9 @@ $(function () {
         $footer = $(".footer");
 
     // Plaxifying stuff.
-    $('#boat').plaxify({"xRange":100,"yRange":20})
+    $('#boat').plaxify({"xRange":100,"yRange":100})
     $('#sun').plaxify({"xRange":15,"yRange":35,"invert":true})
-    $('#birds').plaxify({"xRange":50,"yRange":70, "invert":true})
+    $('#birds').plaxify({"xRange":150,"yRange":100, "invert":true})
     // $('#bg-bottom-initial').plaxify({"xRange":50,"yRange":10,"invert":true})
     $('#clouds').plaxify({"xRange":50,"yRange":10,})
     // $('.title').plaxify({"xRange":30,"yRange":20,"invert":true})
